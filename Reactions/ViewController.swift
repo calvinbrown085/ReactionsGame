81104616c5f357d77a2d23bd7a491ea5189590cd
Calvin Brown <calvinbrown@calvins-macbook-pro.local>
2017-02-17 10:03:41 -0600

    Initial Commit

---
+++ Reactions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reactions
+//
+//  Created by Calvin Brown on 2/17/17.
+//  Copyright © 2017 Calvin Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
